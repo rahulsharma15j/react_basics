28c77c54111241da3221eced0de839803ef5e17a
Rahul Sharma <rahulsharma15j@gmail.com>
2021-02-01 20:51:40 +0530

    JSX array

--- Indecision-app/public/scripts/app.js
+++ Indecision-app/public/scripts/app.js
@@ -31,6 +31,8 @@ var onRemoveAll = function onRemoveAll() {
 
 var appRoot = document.getElementById("app");
 
+var numbers = [55, 101, 1000];
+
 var render = function render() {
   var template = React.createElement(
     "div",
@@ -63,16 +65,13 @@ var render = function render() {
     React.createElement(
       "ol",
       null,
-      React.createElement(
-        "li",
-        null,
-        "Item one"
-      ),
-      React.createElement(
-        "li",
-        null,
-        "Item two"
-      )
+      app.options.map(function (option) {
+        return React.createElement(
+          "li",
+          { key: option },
+          option
+        );
+      })
     ),
     React.createElement(
       "form",
--- Indecision-app/src/app.js
+++ Indecision-app/src/app.js
@@ -29,6 +29,8 @@ const onRemoveAll = () => {
 
 const appRoot = document.getElementById("app");
 
+const numbers = [55, 101, 1000];
+
 const render = () => {
   const template = (
     <div>
@@ -37,9 +39,15 @@ const render = () => {
       <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
       <p>{app.options.length}</p>
       <button onClick={onRemoveAll}>Remove All</button>
+      {
+        //   numbers.map((number) => {
+        //   return <p key={number}>Number: {number}</p>;
+        // })
+      }
       <ol>
-        <li>Item one</li>
-        <li>Item two</li>
+        {app.options.map((option) => {
+          return <li key={option}>{option}</li>;
+        })}
       </ol>
       <form onSubmit={onFormSubmit}>
         <input type="text" name="option" />
