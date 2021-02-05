0328d9f55b06bdb8d2a883593c06754f8cf93cc1
Rahul Sharma <rahulsharma15j@gmail.com>
2021-02-05 21:13:37 +0530

    class inheritence

--- Indecision-app/src/playground/es6-classes-1.js
+++ Indecision-app/src/playground/es6-classes-1.js
@@ -14,10 +14,35 @@ class Person {
   }
 }
 
-const me = new Person("Andrew Mead", 26);
-console.log(me.getGretting());
-console.log(me.getDescription());
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
 
-const other = new Person();
-console.log(other.getGretting());
-console.log(other.getDescription());
+  hasMajor() {
+    return !!this.major;
+  }
+
+  getDescription() {
+    let description = super.getDescription();
+
+    if (this.hasMajor()) {
+      description += ` Their major is ${this.major}.`;
+    }
+    return description;
+  }
+}
+
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+}
+
+const me = new Traveler("Andrew Mead", 26, "Philadelphia");
+console.log(me);
+
+const other = new Traveler();
+console.log(other);
