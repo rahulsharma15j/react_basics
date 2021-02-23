4eb0d9ae364ef6940f889baefb7b5e838fd31cc5
Rahul Sharma <rahulsharma15j@gmail.com>
2021-02-23 23:29:09 +0530

    component state 2

--- Indecision-app/src/playground/counter-example.js
+++ Indecision-app/src/playground/counter-example.js
@@ -10,15 +10,27 @@ class Counter extends React.Component {
     };
   }
   handleAddOne() {
-    console.log("handleAddOne");
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
   }
 
   handleMinusOne() {
-    console.log("handleMinusOne");
+    this.setState((prevState) => {
+      return {
+        count: prevState.count - 1,
+      };
+    });
   }
 
   handleReset() {
-    console.log("handleReset");
+    this.setState(() => {
+      return {
+        count: 0,
+      };
+    });
   }
 
   render() {
