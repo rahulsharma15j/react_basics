7a7413b7a2ac8c4a870bca9e6cee4fafc8e59224
Rahul Sharma <rahulsharma15j@gmail.com>
2021-03-12 20:02:50 +0530

    import

--- Indecision-app/public/bundle.js
+++ Indecision-app/public/bundle.js
@@ -65,9 +65,34 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils_js__ = __webpack_require__(1);
+// import"./utils.js";
+
 
 console.log("app.js is running");
+
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["b" /* square */](4));
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["a" /* add */](4, 5));
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return square; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return add; });
+console.log("utils.js is running");
+
+const square = (x) => x * x;
+
+const add = (a, b) => a + b;
+
+
 
 
 /***/ })
