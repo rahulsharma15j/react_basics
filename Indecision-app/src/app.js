4e0953cfe5588fe322439b34e7740134afc8f6a3
Rahul Sharma <rahulsharma15j@gmail.com>
2021-03-15 00:44:41 +0530

    npm import

--- Indecision-app/src/app.js
+++ Indecision-app/src/app.js
@@ -1,14 +1,6 @@
-// import"./utils.js";
-// import subtract, { square, add } from "./utils.js";
+import React from "react";
+import ReactDOM from "react-dom";
 
-// console.log("app.js is running");
+const template = React.createElement("p", {}, "testing 123");
 
-// console.log(square(4));
-// console.log(add(4, 5));
-// console.log(subtract(4, 5));
-
-import isSenior, { isAdult, canDrink } from "./person.js";
-
-console.log(isAdult(20));
-console.log(canDrink(20));
-console.log(isSenior(80));
+ReactDOM.render(template, document.getElementById("app"));
