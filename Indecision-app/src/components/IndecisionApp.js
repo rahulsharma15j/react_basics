8ce0738d33fe2fb8186e98e5cb2392a06d59641c
Rahul Sharma <rahulsharma15j@gmail.com>
2021-03-19 22:55:57 +0530

    scss variables

--- Indecision-app/src/components/IndecisionApp.js
+++ Indecision-app/src/components/IndecisionApp.js
@@ -70,17 +70,20 @@ export default class IndecisionApp extends React.Component {
     const subtitle = "Put your life in the hands of a computer.";
     return (
       <div>
-        <Header title={title} subtitle={subtitle} />
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption handleAddOption={this.handleAddOption} />
+        <Header subtitle={subtitle} />
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption handleAddOption={this.handleAddOption} />
+        </div>
+
         <OptionModal
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
