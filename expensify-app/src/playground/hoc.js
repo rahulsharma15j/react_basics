813e9040d9b244d729ab3c04c3c5a5049bab696a
Rahul Sharma <rahulsharma15j@gmail.com>
2021-05-18 20:38:37 +0530

    hoc

--- expensify-app/src/playground/hoc.js
+++ expensify-app/src/playground/hoc.js
@@ -1,20 +1,18 @@
 // Higher Order Component (HOC) - A component that renders another component
 import React from "react";
-
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 
 const Info = (props) => (
   <div>
     <h1>Info</h1>
-    <p>The info is: {props.info}</p>
+    <p>The info is:{props.info}</p>
   </div>
 );
 
 const withAdminWarning = (WrappedComponent) => {
   return (props) => (
     <div>
-      {props.isAdmin && <p>This is private info please don't share!</p>}
-
+      {props.isAdmin && <p>This is private info.Please don't share!</p>}
       <WrappedComponent {...props} />
     </div>
   );
@@ -33,10 +31,14 @@ const requireAuthentication = (WrappedComponent) => {
 };
 
 const AdminInfo = withAdminWarning(Info);
-
 const AuthInfo = requireAuthentication(Info);
 
-ReactDom.render(
-  <AuthInfo isAuthenticated={false} info="There are the details" />,
+// ReactDOM.render(
+//   <AdminInfo isAdmin={true} info="These are the detail" />,
+//   document.getElementById("app")
+// );
+
+ReactDOM.render(
+  <AuthInfo isAuthenticated={true} info="These are the detail" />,
   document.getElementById("app")
 );
